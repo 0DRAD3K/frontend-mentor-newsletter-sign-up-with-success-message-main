d06e9811c338d4c63db0ced441c0d2c13c9dd3c6
0DRADEK <104868135+meursault88@users.noreply.github.com>
2023-08-13 18:42:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const form = document.querySelector('form');
 const card1 = document.querySelector('.card');
-const card2 = document.querySelector('.card_thanks')
+const card2 = document.querySelector('.card_thanks');
+const dismiss = document.querySelector('.card_thanks button');
 form.addEventListener('submit' , (e) => {
     e.preventDefault();
     card1.classList.add("hide");
     card2.classList.remove('hide');
+});
+dismiss.addEventListener('click', (e) => {
+    e.preventDefault();
+    card1.classList.add("hide");
+    card2.classList.add("hide");
 });
